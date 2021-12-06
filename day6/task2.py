d320ce72e24aa1996e28d150d705e8e63ee4cee2
Andreas Bruckmeier <andreas.bruckmeier@neteleven.de>
2021-12-06 07:05:18 +0100

    Clean up solutions

--- day6/task2.py
+++ day6/task2.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 with open('input.txt', 'r') as f:
-  data = list(map(str, f))
 
-fishes = [int(x) for x in data[0].split(',')]
+  fishes = [int(x) for x in f.readline().split(',')]
 
-timers = [0,0,0,0,0,0,0,0,0]
+  timers = [0]*9
 
-for fish in fishes:
-  timers[fish] += 1
+  for fish in fishes:
+    timers[fish] += 1
 
-for i in range(256):
-  add_new = timers[0]
-  reset = timers[0]
-  for j in range(len(timers)-1):
-    timers[j] = timers[j+1]
-    if j == 6:
-      timers[j] += reset
-  timers[8] = add_new
+  for i in range(256):
+    add_new = timers[0]
+    reset = timers[0]
+    for j in range(len(timers)-1):
+      timers[j] = timers[j+1]
+      if j == 6:
+        timers[j] += reset
+    timers[8] = add_new
 
-print(f"task 2: {sum(timers)}")
+  print(f"task 2: {sum(timers)}")
