f95064f54a46b93b39455fb9678cefef92b1a1c2
Andreas Bruckmeier <andreas.bruckmeier@neteleven.de>
2021-12-12 09:04:39 +0100

    Day 12 without node class

--- day12/task1.py
+++ day12/task1.py
@@ -1,64 +1,35 @@
 #!/usr/bin/env python3
-class Cave:
+from collections import defaultdict
 
-    def __init__(self, name):
-        self.name = name
-        self.adjacent_caves = []
+def find_paths(cave, second_visit = None, path = []):
 
-    def add_adjacent(self, cave):
-        if not cave in self.adjacent_caves:
-            self.adjacent_caves.append(cave)
-
-    def get_adjacent(self):
-        return self.adjacent_caves
-
-    def is_small(self):
-        return self.name not in ['start', 'end'] \
-          and self.name.lower() == self.name
-
-def solve(cave, second_visit = None, path = []):
-
-    # dont visit small caves more than once 
-    # except we have a joker (second_visit)
-    if cave.is_small() and cave in path:
+    if cave.islower() and cave not in ['start', 'end'] and cave in path:
         if cave != second_visit:
             return
         second_visit = None
 
-    # if we hit the end, we add this path as
-    # a tuple to the paths set
-    if cave.name == 'end':
-        paths.add(tuple([x.name for x in path]))
+    if cave == 'end':
+        paths.add(tuple(path))
         return
 
-    for adjacent in cave.get_adjacent():
-        if adjacent.name == 'start':
-            continue
-        solve(adjacent, second_visit, path + [cave])
+    for adjacent in [c for c in caves[cave] if c != 'start']:
+        find_paths(adjacent, second_visit, path + [cave])
 
 with open('input.txt', 'r') as f:
 
-    # Build cave tree
-    caves = {}
-    for line in f.read().strip().split("\n"):
-        cfrom, cto= line.split('-')
-        if cfrom not in caves:
-            node = Cave(cfrom)
-            caves[cfrom] = node
-        if cto not in caves:
-            node = Cave(cto)
-            caves[cto] = node
-        caves[cto].add_adjacent(caves[cfrom])
-        caves[cfrom].add_adjacent(caves[cto])
+    caves = defaultdict(set)
+    for path_from, path_to in [x.split('-') for x in f.read().strip().split("\n")]:
+        caves[path_to].add(path_from)
+        caves[path_from].add(path_to)
 
     # task 1
     paths = set()
-    solve(caves['start'])
+    find_paths('start')
     print(f"Task 1:  {len(paths)} paths")
 
     # task 2
     paths = set()
-    for name, cave in caves.items():
-        if len(name) not in ['start', 'end'] and name.lower() == name:
-            solve(caves['start'], cave)
+    for cave in caves:
+        if cave.islower() and cave not in ['start', 'end']:
+            find_paths('start', cave)
     print(f"Task 2:  {len(paths)} paths")
